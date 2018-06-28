7dbbae8790f0afe8383de6affda534d036e71abd
hotmix <dai@hotmix.jp>
2018-06-28 09:55:03 +0900

    Initial commit

---
+++ src/main/java/jp/hotmix/mediasample/MediaControlActivity.java
@@ -0,0 +1,13 @@
+package jp.hotmix.mediasample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MediaControlActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_media_control);
+    }
+}
